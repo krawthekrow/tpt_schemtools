e2a90aced4e36a9b11e4e738194ddd0d84a28004
krawthekrow <krawthekrow@gmail.com>
2022-02-11 14:22:52 -0500

    stdlib: complete 1d filt rom example

--- schematics/main.lua
+++ schematics/main.lua
@@ -1,11 +1,38 @@
 require('schematics/stdlib')
 
 return function(SchemTools)
-	local demux_inst = schem(function()
-		pstn_demux{}
+	schem{
+		f=pstn_demux,
+		v='demux_1',
+		x=100, y=100,
+		detach_pscn_placer=1,
+	}
+	port{v='addr_in', p=v('demux_1.addr_in'):left(5)}
+	filt{p=v('addr_in'), ct=bor(5, ka)}
+	connect{p1='addr_in', p2='demux_1.addr_in'}
+	chain({dx=1, p=v('demux_1.pstn_head')}, function()
+		frme{done=0}
+		frme{oy=1}
+
+		port{v='data_w', oy=-2}
+		ldtc{r=1, j=1, done=0}
+		filt{oy=1}
 	end)
-	place(demux_inst, {x=100, y=100, name='demux_1'})
-	filt{p=v('demux_1.addr_in'), ct=bor(5, ka)}
+
+	chain({dx=1, p=v('data_w')}, function()
+		for i = 1, 32 do
+			if i == 32 then port{v='data_e'} end
+			filt{ct=bor(ka, i)}
+		end
+	end)
+
+	port{v='demux_pscn_placer_x', p=v('data_e'):right()}
+	connect{p1='demux_1.pscn_placer', p2='demux_pscn_placer_x'}
+
+	-- connect{p1='demux_1.pscn_placer', p2='demux_1.pstn_head'}
+	-- port{v='demux_head', p=findpt{
+	-- 	e=v('demux_1.pstn_head'), ns=v('demux_1.pscn_placer_e')
+	-- }}
 	clear{w=200, h=200}
 	plot{}
 end
--- schematics/stdlib.lua
+++ schematics/stdlib.lua
@@ -1,99 +1,150 @@
 function ssconv(opts)
-	opts = opts_bool(opts, 'done', true)
-	opts = opts_pos(opts)
-	conv{p = opts.p, from='sprk', to=opts.t, done=0}
-	conv{p = opts.p, from=opts.t, to='sprk', done=opts.done}
+	conv{from='sprk', to=opts.t}
+	conv{from=opts.t, to='sprk'}
 end
 
 function pstn_demux(opts)
+	opts_bool(opts, 'detach_pscn_placer', false)
 	if opts.n == nil then opts.n = 32 end
-	if opts.xn == nil then opts.xn = 0 end
 	assert(opts.n >= 1)
 	assert(opts.n <= 32)
 	local num_segs = ilog2(opts.n)
 
-	-- set up bit checking logic
-	local arayy, inputp, pscn_s, pscn_e, pstn_s, pstn_e
-	chain({dx=1}, function()
-		for i = 1, num_segs do
-			chain({dx=1, dy=-1}, function()
-				if i == 1 then pscn_s = getcurs() end
-				if i == num_segs then pscn_e = getcurs() end
-				insl{}
-				if i == 1 then inputp = getcurs() end
-				filt{tmp=fsub}
-				filt{tmp=fset, ct=shl(1, num_segs-i)}
-				if i == 1 then arayy = getcurs().y end
-				aray{done=0}
-				ssconv{t='inwr'}
-				inwr{sprk=1}
-			end)
-		end
+	-- bit checking logic
+	array(num_segs, {dx=1}, function(i)
+		chain({dx=-1, dy=1}, function()
+			inwr{sprk=1}
+			if i == 1 then port{v='arayrow_w'} end
+			-- we want the BRAY to persist to the next frame so that the
+			-- resetter CRAY always deletes the same number of particles
+			aray{life=2, done=0}; schem{f=ssconv, t='inwr'}
+			filt{tmp=fset, ct=shl(1, num_segs-i)}
+			if i == 1 then port{v='addrrow_w'} end
+			filt{tmp=fsub}
+			if i == 1 then port{v='pscnrow_w'} end
+			if i == num_segs then port{v='pscnrow_e'} end
+			insl{}
+		end)
 	end)
 
-	local retractor_target
-	chain({dx=-1, p=pscn_e:add(p(2, 1))}, function()
-		port('head')
-		adv{}
-		pstn{r=1 + opts.xn}
+	-- piston, binary section
+	chain({dx=-1, p=v('pscnrow_e'):down()}, function()
+		-- each PSTN's pushing power includes all the PSTNs in front of it,
+		-- so subtract the accumulated pushing power from the target
+		-- pushing power
 		local cum_r = 0
 		for i = 1, num_segs do
-			if i == 1 then pstn_s = getcurs() end
-			if i == num_segs then pstn_e = getcurs() end
+			if i == 1 then port{v='pstn_bin_e'} end
+			if i == num_segs then port{v='pstn_bin_w'} end
 			local target_r = shl(1, i - 1)
 			pstn{r=target_r - cum_r, ct='dmnd'}
 			cum_r = target_r
 		end
-		retractor_target = getcurs()
+	end)
+
+	-- piston front
+	chain({dx=1, p=v('pstn_bin_e'):right()}, function()
+		pstn{r=1}
+		port{v='pstn_head'}
+	end)
+
+	-- piston back
+	chain({dx=-1, p=v('pstn_bin_w'):left()}, function()
+		port{v='pstn_target'}
+		port{v='apom_pstn_id_holder', oy=1}
 		adv{}
 		dmnd{}
 	end)
 
-	-- extend addr_in ldtc line
-	local ldtc_target
-	chain({dx=-1, p=inputp}, function()
-		adv{}
-		filt{}
-		ldtc_target = getcurs()
-		adv{}
-		adv{}
-		port('addr_in')
-	end)
+	-- PSCN placer to activate PSTN where BRAY is annihilated
+	local function make_pscn_placer(p, ss_to_right)
+		chain({dx=1, p=p}, function()
+			pscn{sprk=1, done=0}
+			if ss_to_right then
+				schem{f=ssconv, t='pscn', ox=1, oy=1}
+			else
+				schem{f=ssconv, t='pscn', ox=-1, oy=1, under=1}
+			end
+			adv{}
+			for i = 1, 5 do
+				dray{r=2, toe=v('pscnrow_e'):left(i-1), done=0}
+			end
+			adv{}
+			schem{f=ssconv, t='inwr', done=0}
+			port{v='pscn_placer_e'}
+			inwr{sprk=1}
+		end)
+	end
 
-	-- place ss pscns in empty locations
-	chain({dx=1, p=pscn_e}, function()
-		adv{}
-		adv{}
-		pscn{sprk=1}
-		adv{}
-		for i = 1, 5 do
-			dray{r=2, to=pscn_e:add(p(1-(i-1), 0)), done=0}
+	port{v='pscn_placer_wbnd', p=v('pscnrow_e'):right(2)}
+	if opts.detach_pscn_placer then
+		port{v='pscn_placer', f=function(opts)
+			make_pscn_placer(findpt{ns=opts.p, ei=v('pscn_placer_wbnd')}, true)
+		end}
+	else
+		make_pscn_placer(v('pscn_placer_wbnd'), false)
+	end
+
+	-- addr_in feed into addr row
+	port{v='ldtc_target', p=findpt{n=v('pstn_target'), w=v('addrrow_w')}}
+	chain({dx=1, p=v('ldtc_target'):right()}, function()
+		while not getcurs():eq(v('addrrow_w')) do
+			filt{}
 		end
-		adv{}
-		ssconv{t='inwr', done=0}
-		inwr{sprk=1}
 	end)
+	port{v='addr_in', p=v('ldtc_target'):left(2)}
 
-	local cray_target = p(ldtc_target.x, pscn_s.y)
-
-	-- set up apom
-	chain({dy=-1, x=ldtc_target.x, y=arayy}, function()
-		insl{}
-		local insl_s = getcurs()
-		insl{}
+	-- setter mechanism for APOM
+	port{v='cray_target', p=findpt{n=v('pstn_target'), w=v('pscnrow_w')}}
+	port{v='apom_setter_s', p=findpt{n=v('cray_target'), w=v('arayrow_w')}}
+	chain({dy=-1, p=v('apom_setter_s')}, function()
+		port{v='apom_insl_s'}
+		insl{} -- holds the LDTC's ID
+		port{v='apom_insl_n'}
+		insl{} -- holds the CRAY's ID
+		chain({p=v('apom_pstn_id_holder'), done=0}, function()
+			insl{} -- holds the PSTN's ID
+		end)
 		pstn{r=0}
-		cray{r=num_segs, from=cray_target, to=pscn_s}
-		cray{r=2, to=insl_s, done=0}
-		cray{r=1, ct='ldtc', to=ldtc_target, done=0}
-		dray{r=2, to=cray_target, done=0}
-		ssconv{ox=1, t='pscn'}
+		cray{r=num_segs, from=v('cray_target'), to=v('pscnrow_w')}
+
+		cray{r=1, to=v('apom_pstn_id_holder'), done=0}
+		cray{s=v('apom_insl_n'), r=2, done=0}
+		cray{r=1, v='apom_ldtc_maker', ct='ldtc', to=v('ldtc_target'), done=0}
+		dray{r=2, tos=v('cray_target'), done=0}
+		schem{f=ssconv, ox=1, t='pscn'}
 		pscn{sprk=1}
 	end)
 
-	-- activate the apom'ed cray
-	chain({p=cray_target}, function()
-		adv{-1, 0}
-		ssconv{t='pscn'}
+	-- Config port for the addr_in reader.
+	-- This is complicated by the fact that the LDTC is created by a CRAY
+	-- through APOM. The LDTC's skip distance is configured through its
+	-- life, which is set by the CRAY's life.
+	port{v='addr_in', p=v('ldtc_target'):left(), done=0, f=function(opts)
+		v('apom_ldtc_maker').life = get_orth_dist(v('ldtc_target'), opts.p) - 1
+	end}
+
+	-- sparker for the APOM'ed CRAY
+	chain({p=v('cray_target'):left()}, function()
+		schem{f=ssconv, t='pscn', done=0}
 		pscn{sprk=1}
+	end)
+
+	-- resetter mechanism for APOM
+	chain({dy=1, p=v('apom_pstn_id_holder'):down()}, function()
+		cray{r=1, to=v('pstn_target'), done=0}
+		dray{r=2, tos=v('cray_target'), done=0}
+		cray{s=v('apom_insl_s'), r=2, ct='insl', done=0}
+		cray{s=v('apom_pstn_id_holder'), r=1, ct='insl'}
+		pscn{sprk=1}
+		-- put the ssconv below since the earlier space is used
+		-- for the resetter PSTN sparker
+		schem{f=ssconv, t='pscn'}
+	end)
+
+	-- spark resetter PSTN
+	chain({dy=1, p=v('pstn_target'):down(2)}, function()
+		nscn{sprk=1}
+		schem{f=ssconv, t='nscn', under=1}
 	end)
 end
